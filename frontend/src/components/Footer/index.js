f78fcd17fd255cbf49755cc0216aa13c28a98c89
Luis <sans.boba@gmail.com>
2022-07-01 14:42:04 -0700

    add rel to a tags

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="footer-bar">
-      <a href="https://www.github.com/stuffy-doll" target="_blank">Luis' Github Page</a>
-      <a href="https://www.github.com/stuffy-doll/lonote" target="_blank">Project Repo</a>
-      <a href="https://www.linkedin.com/in/luis-sanchez-b96077235/" target="_blank">Luis' LinkedIn</a>
+      <a href="https://www.github.com/stuffy-doll" target="_blank" rel="noreferrer">Luis' Github Page</a>
+      <a href="https://www.github.com/stuffy-doll/lonote" target="_blank" rel="noreferrer">Project Repo</a>
+      <a href="https://www.linkedin.com/in/luis-sanchez-b96077235/" target="_blank" rel="noreferrer">Luis' LinkedIn</a>
     </div>
   );
 };
